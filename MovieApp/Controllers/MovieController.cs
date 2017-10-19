9d7f3fc0496a39f922032168158b30a0929f9fea
andrujoh <andru_joh@yahoo.no>
2017-10-19 22:53:51 +0200

    Add movies page

---
+++ MovieApp/Controllers/MovieController.cs
@@ -0,0 +1,43 @@
+﻿using MovieApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieApp.Controllers
+{
+  public class MovieController : Controller
+  {
+    private static List<string> db = new List<string>
+    {
+      "Iron Man",
+      "Justice League"
+    };
+
+    // GET: Movie
+    public ViewResult Index(string search)
+    {
+      var movies = (search == null)
+        ? db.ToArray()
+        : db.Where(name => name.ToLower().Contains(search.Trim().ToLower()))
+        .ToArray();
+
+      var viewModel = new MovieSearchViewModel
+      {
+        Search = search,
+        Movies = movies
+      };
+      return View(viewModel);
+    }
+
+    [HttpPost]
+    [Authorize]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(string movie)
+    {
+      db.Add(movie);
+      return RedirectToAction("Index");
+    }
+  }
+}
---
+++ MovieApp/Models/MovieSearchViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieApp.Models
+{
+  public class MovieSearchViewModel
+  {
+    public string Search { get; set; }
+    public string[] Movies { get; set; }
+  }
+}
